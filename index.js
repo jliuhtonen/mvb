7410e3ba578eccfe4f08c2f775938f39253ea8f6
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2015-07-12 18:59:59 +0300

    Do not initialize on module load

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const telegram = require('./mvb')
+const Mvb = require('./mvb')
 const moment = require('moment')
 const jgtDate = moment("2014-05-14")
 
-telegram.onMessage('/jgt', (message, reply) => {
+const bot = new Mvb()
+
+bot.onMessage('/jgt', (message, reply) => {
   const daysSinceJgt = moment().diff(jgtDate, 'days')
   const daysSinceJgtMsg = `${daysSinceJgt} päivää JGT`
   reply(daysSinceJgtMsg)
--- mvb.js
+++ mvb.js
@@ -7,64 +7,71 @@ const apiUrl = `https://api.telegram.org/bot${config.authToken}`
 const updatesUrl = `${apiUrl}/getUpdates`
 const sendMessageUrl = `${apiUrl}/sendMessage`
 
-const sendMessage = (chatId, message) => {
+function sendMessage(chatId, message) {
   const text = encodeURIComponent(message)
   const url = `${sendMessageUrl}?chat_id=${chatId}&text=${text}`
   request.post(url).end()
 }
 
-const getUpdates = (sinceUpdateId, callback) => {
+function getUpdates(sinceUpdateId, callback) {
   const url = `${updatesUrl}?timeout=${longPollDurationSeconds}&offset=${sinceUpdateId}`
   request.get(url).end(callback)
 }
 
-const latestUpdateIds = new Bacon.Bus()
-const lastUpdateId = latestUpdateIds.toProperty(0)
+function Mvb() {
 
-const updateResponses = lastUpdateId.flatMapLatest((lastUpdate) => Bacon.fromNodeCallback(getUpdates, lastUpdate))
+  const latestUpdateIds = new Bacon.Bus()
+  const lastUpdateId = latestUpdateIds.toProperty(0)
 
-updateResponses.onError(console.error.bind(this))
+  const updateResponses = lastUpdateId.flatMapLatest((lastUpdate) => Bacon.fromNodeCallback(getUpdates, lastUpdate))
 
-const updates = updateResponses.map((res) => res.body)
-const okUpdates = updates.filter((resp) => resp.ok)
+  updateResponses.onError(console.error.bind(this))
 
-const updateIdWithResponse = lastUpdateId.combine(okUpdates, (id, response) => {
-  return {
-    id: id,
-    response: response
-  }
-}).changes()
+  const updates = updateResponses.map((res) => res.body)
+  const okUpdates = updates.filter((resp) => resp.ok)
 
-updateIdWithResponse.log()
+  const updateIdWithResponse = lastUpdateId.combine(okUpdates, (id, response) => {
+    return {
+      id: id,
+      response: response
+    }
+  }).changes()
 
-const receivedLatestUpdateIds = updateIdWithResponse.map((responseData) => {
-  const { id, response } = responseData
-  const resultCount = response.result.length
+  const receivedLatestUpdateIds = updateIdWithResponse.map((responseData) => {
+    const { id, response } = responseData
+    const resultCount = response.result.length
 
-  if (resultCount > 0) { 
-    return response.result[resultCount - 1].update_id + 1
-  } else {
-    return id
-  }
-})
+    if (resultCount > 0) { 
+      return response.result[resultCount - 1].update_id + 1
+    } else {
+      return id
+    }
+  })
 
-const currentUpdateIdOnError = updateResponses.errors().mapError(lastUpdateId)
-const nextUpdateIdOnResponse = receivedLatestUpdateIds.merge(currentUpdateIdOnError)
-latestUpdateIds.plug(nextUpdateIdOnResponse)
+  const currentUpdateIdOnError = updateResponses.errors().mapError(lastUpdateId)
+  const nextUpdateIdOnResponse = receivedLatestUpdateIds.merge(currentUpdateIdOnError)
+  latestUpdateIds.plug(nextUpdateIdOnResponse)
 
-const incomingResults = okUpdates
-  .filter((update) => update.result && update.result.length > 0)
-  .flatMapLatest((update) => Bacon.fromArray(update.result))
+  const incomingResults = okUpdates
+    .filter((update) => update.result && update.result.length > 0)
+    .flatMapLatest((update) => Bacon.fromArray(update.result))
 
-const incomingMessages = incomingResults.filter((result) => !!result.message)
+  const incomingMessages = incomingResults.filter((result) => !!result.message)
 
-module.exports = {
-  onMessage: (value, f) => {
+  function onMessage(value, handler) {
     return incomingMessages
       .filter((result) => result.message.text.trim() === value)
       .onValue((result) => {
-        f(result.message.text, (replyMsg) => sendMessage(result.message.chat.id, replyMsg))
+	const replyFunction = (replyMsg) => sendMessage(result.message.chat.id, replyMsg)
+	handler(result.message.text, replyFunction) 
       })
-    },
-  sendMessage: sendMessage
+  }
+
+  return {
+    onMessage: onMessage,
+    sendMessage: sendMessage
+  }
+
 }
+
+module.exports = Mvb
