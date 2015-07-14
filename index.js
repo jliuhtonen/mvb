f913957a7e70533f6b7f4b8db64f7518b8ee77ec
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2015-07-14 22:15:04 +0300

    jgt timestamp feature

--- index.js
+++ index.js
@@ -5,10 +5,26 @@ const jgtDate = moment("2014-05-14")
 const bot = new Mvb()
 
 bot.onCommand('jgt', (args, reply) => {
-  console.log(args)
-  const daysSinceJgt = moment().diff(jgtDate, 'days')
-  const daysSinceJgtMsg = `${daysSinceJgt} päivää JGT`
-  reply(daysSinceJgtMsg)
+  const [command] = args
+  const timeUnit = getTimeUnit(command)
+  const timeSinceJgt = moment().diff(jgtDate, timeUnit)
+  reply(createReplyMessage(timeSinceJgt, timeUnit))
 })
+
+function getTimeUnit(command) {
+  if (command === 'timestamp') {
+    return 'millis'
+  } else {
+    return 'days'
+  }
+}
+
+function createReplyMessage(amount, unit) {
+  if(unit === 'millis') {
+    return `JGT timestamp ${amount}`
+  } else {
+    return `${amount} päivää JGT`
+  }
+}
 
 console.log("JGT bot 3000 starting!!!!!")
